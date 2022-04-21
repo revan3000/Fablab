879e3124a33308dd04402a6edc02a8ed02d57116
revan3000 <eliot.bonni3000@gmail.com>
2022-04-21 15:16:39 -0400

    update

---
+++ condicionales.py
@@ -0,0 +1,11 @@
+a=100
+b=1000
+if a > b:
+	print ('a es mayor que b')
+elif a == b:
+	print('a es igual a b')
+else:
+	print('b es mayor que a')
+if a > b: print ('a es mayor que b')
+print ('a es mayor que b') if a > b else print ('b es mayor que a')	
+print ('a') if a > b else print ('=') if a == b else print ('b')
