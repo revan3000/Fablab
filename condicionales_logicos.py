2e79ab0b28cd923f7c99ce036905254a3d18c86c
revan3000 <eliot.bonni3000@gmail.com>
2022-04-21 15:49:08 -0400

    update

---
+++ condicionales_logicos.py
@@ -0,0 +1,7 @@
+a=1000
+b=10000
+c=100000
+if a > b and a > c:
+	print ('a es superior a todo')
+if a> b or c > b:
+	print ('1 sola verdad')
---
+++ validador_de_datos.py
@@ -0,0 +1,20 @@
+edad=input ('que edad tienes ?')
+edad=int(edad)
+if edad < 18 or edad > 60:
+	print ('usted no tiene la edad requerida para entrar')
+else:
+	print ('bienvenido a la pagina web')
+if edad >= 18:
+	print ('menu')
+	x=['vino','cerveza']
+	for b in x :
+		print (b)
+	usuario=input ('que desea tomar ?')
+	if usuario==x[0]:
+		print ('*entregar copa de vino*')
+	elif usuario==x[1]:
+		print ('*entregar vaso de cerveza*')
+	else:
+		print ('esta opcion no esta disponoble en este bar')
+else:
+	print ('no tienes la edad para entrar')
